6665d7a7884781a565c28ddd5f83c5042061fe45
Nuristiana <nuristianaizatul@gmail.com>
2025-10-23 17:53:33 +0700

    CRUD Service

--- services/engineering-service/src/app.ts
+++ services/engineering-service/src/app.ts
@@ -1,10 +1,14 @@
 import express = require('express');
 import materialsRoutes from './routes/materialsRoutes';
+import serviceRoutes from './routes/serviceRoutes';
 
 const app = express();
 app.use(express.json());
 
 // mount engineering-related routes (materials)
 app.use('/', materialsRoutes);
+
+// mount engineering-related routes (services)
+app.use('/', serviceRoutes);
 
 export default app;
