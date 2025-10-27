5bad6d378b90ec71a7cb58560740d9e9564ba046
Syifa Setiawati <septianifirsa77@gmail.com>
2025-10-27 15:55:46 +0700

    Save current changes before merge

--- services/crm-service/src/utils/prisma.ts
+++ services/crm-service/src/utils/prisma.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-  log: ['query', 'error', 'warn'],
+  log: ["query", "error", "warn"],
 });
 
 export default prisma;
